e89fc193c836b91d0df6825586b59daca4a12421
cuki2 <mauricio@krebs.com.br>
2017-05-29 16:03:59 -0300

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- RTCC.X/rtcc.c
+++ RTCC.X/rtcc.c
@@ -2,14 +2,15 @@
 #include "rtcc.h"
 
 
-void RTCC_init(void) {
+void RTCC_init(void)
+{
     __builtin_write_RTCC_WRLOCK(); // Clear WRLOCK to modify RTCC as needed
 
-    RTCCON1Lbits.OUTSEL=0b001; // 0b001 -> second clock
-    //RTCCON1Lbits.OUTSEL = 0b000; // 0b000 -> alarm event
+    RTCCON1Lbits.OUTSEL = 1; // 0b001 -> second clock
+    //RTCCON1Lbits.OUTSEL = 0; // 0b000 -> alarm event
     RTCCON1Lbits.RTCOE = 1; // RTCC pin pulses for debugging
-    //RTCCON2Lbits.CLKSEL=0b01; // 0b01 -> LPRC
-    RTCCON2Lbits.CLKSEL = 0b00; // 0b00 -> SOSC
+    RTCCON2Lbits.CLKSEL = 1; // 0b01 -> LPRC
+    //RTCCON2Lbits.CLKSEL = 0; // 0b00 -> SOSC
 
     // Set some random times
     if ((DATEH & 0xFF1F) == 0x0001) // Reset value
@@ -24,6 +25,7 @@ void RTCC_init(void) {
     RTCCON1Hbits.AMASK = 0b0000; // 0b0000 -> every 1/2 sec
     RTCCON1Hbits.CHIME = 1;
     RTCCON1Hbits.ALRMEN = 1;
+    RTCCON1Hbits.ALMRPT = 2;
     IFS3bits.RTCIF = 0;
     IEC3bits.RTCIE = 1;
     RTCCON1Lbits.RTCEN = 1;
@@ -31,7 +33,8 @@ void RTCC_init(void) {
 
 }
 
-void RTCC_reset(uint8_t u8) {
+void RTCC_reset(uint8_t u8)
+{
     __builtin_write_RTCC_WRLOCK();
     TIMEL = 0; // Reset seconds
     TIMEH = (TIMEH & 0xFF00) | u8;
--- RTCC.X/rtcc.h
+++ RTCC.X/rtcc.h
@@ -1,8 +1,14 @@
 #ifndef RTCC_H
 #define RTCC_H
 
+#include "sys.h"
+
 #include <xc.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+typedef struct rtcc_str {
+};
 
 void RTCC_init(void);
 void RTCC_reset(uint8_t u8);
